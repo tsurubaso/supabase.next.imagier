ac4d8fc5d6e0eb2397519d026e9ac0030a86d533
tsurubaso <tsurubaso@hotmail.com>
2025-02-07 09:02:42 +0900

    Update page.jsx
    Trying something to get page shown 1

--- app/book/[title]/page.jsx
+++ app/book/[title]/page.jsx
@@ -18,7 +18,7 @@ const BookPage = ({ params }) => {
       if (!title) return; // Skip if there's no title
 
       try {
-        const res = await fetch(`/books/${title}.md`);
+        const res = await fetch(`/af6daca/${title}.md`);
         if (!res.ok) {
           throw new Error(`Failed to fetch the book: ${title}`);
         }
